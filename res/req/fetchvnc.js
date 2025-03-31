6322cec58b104ddda2edb8c8ae721a4d1baa7281
imguest24897-alt <161818573+imguest24897-alt@users.noreply.github.com>
2025-03-31 10:29:19 +0300

    a litte update

--- res/req/fetchvnc.js
+++ res/req/fetchvnc.js
@@ -22,7 +22,7 @@ function updateVNC() {
     if (vncList.length === 0) return;
 
     const currentVNC = vncList[currentIndex];
-    const validStatuses = [null, 'Up', 'Down', 'Up (responded to ping)', 'Down (did not respond to ping)', 'Down (could not connect to server)'];
+    const validStatuses = [null, 'Up', 'Down', 'Up (responded to ping)', 'Down (did not respond to ping)', 'Down (could not connect to server)', 'Up (unknown)', 'Down (unknown)'];
     let status;
 
     if (validStatuses.includes(currentVNC.status)) {
